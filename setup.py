52043f33912455b778517bf61a841049ce717288
Sergi Castillo Tiñena <sergicastillo@hotmail.es>
2024-04-18 16:41:16 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nombre_del_paquete',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'coverage',
+        'pylint',
+        'pytest',
+        'ggshield',
+        'psycopg2',
+        'flask',
+        'json-log-formatter',
+        'twine'
+              
+    ],
+    author='Sergi Castillo',
+    description='KeepCoding',
+    url='https://github.com/sergicastin/CI-CD-SergiCastillo',
+)
