3fce79db425fd532e3db25ffb85c336aa1b21f65
Sergi Castillo Tiñena <sergicastillo@hotmail.es>
2024-04-18 16:48:13 +0200

    D4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nombre_del_paquete',
+    name='CI-CD-SergiCastillo',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
